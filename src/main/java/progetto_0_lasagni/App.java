199a2a1931b8d7be65955c18cf4ce87a666ea696
lai2-01 <lai2-01@itismeucci.com>
2023-09-18 11:46:56 +0200

    pm

---
+++ src/main/java/progetto_0_lasagni/App.java
@@ -0,0 +1,13 @@
+package progetto_0_lasagni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
